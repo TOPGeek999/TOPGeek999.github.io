83e33dc94fa7418ec14eb475aa8e269222cdda3e
TOPGeek999 <123210777+TOPGeek999@users.noreply.github.com>
2024-11-25 01:07:52 +0200

    Add files via upload

---
+++ puslapis/functions.js
@@ -0,0 +1,17 @@
+const mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.opacity = "1";
+    document.getElementById("myBtn").style.bottom = "20px";
+  } else {
+    document.getElementById("myBtn").style.opacity = "0";
+    document.getElementById("myBtn").style.bottom = "-10px";
+  }
+}
+
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
