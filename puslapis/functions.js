30a6de9441e59ff5a7dd7b2973b7dfb73fe32894
TOPGeek999 <123210777+TOPGeek999@users.noreply.github.com>
2024-12-02 00:08:53 +0200

    Add files via upload

--- puslapis/functions.js
+++ puslapis/functions.js
@@ -1,3 +1,7 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
 const mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction();};
